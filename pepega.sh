8020c2bc0886bf074912afec7bab17062ac02a70
Nydia Might <nydiamig@un-m6.kzn.21-school.ru>
2023-10-09 22:49:47 +0300

    Sanya nichego ne robit

--- pepega.sh
+++ pepega.sh
@@ -17,42 +17,71 @@ cd /Users/$(whoami)/Downloads
 
 # afplay [option...] audio_file
 
-# Папка, в которой будут создаваться случайные папки
+# Функция для генерации случайной буквы a-z
+bash 
+generate_random_letter() {
+    letters="abcdefghijklmnopqrstuvwxyz"
+    random_index=$((RANDOM % ${#letters}))
+    random_letter="${letters:random_index:1}"
+    echo "$random_letter"
+  }
+
+generate_random_script(){
+  num_scripts=3
+  for ((i=1; i<=num_scripts; i++)); do
+    # Генерируем случайное имя файла
+      for ((j=1; j<=8; j++)); do
+      random_script_name="$random_script_name$(generate_random_letter)"
+      done
+
+    # Полный путь к новому скрипту
+      new_script_path="$new_script/$random_script_name.sh"
+
+    # Создаем файл скрипта с случайным именем
+      touch "$new_script_path"
+
+    # Добавляем содержимое скрипта
+      echo "#!/bin/bash" >> "$new_script_path"
+      echo "echo 'Hello, World!'" >> "$new_script_path"
+
+    # Делаем скрипт исполняемым
+      chmod +x "$new_script_path"
+
+
+    # Создаем случайные скрипты 
+
+    # Запускаем скрипт
+      "$new_script_path"
+        echo "Создана папка: $new_folder"
+  done
+
+  echo "Создано $num_folders случайных папок в $base_folder"
+}
+
+generate_random_folder(){
+  # Папка, в которой будут создаваться случайные папки
 base_folder="/Users/$(whoami)/Desktop/test"
 
 # Количество случайных папок, которые вы хотите создать
-num_folders=5
+num_folders=3
 
 # Создать случайные папки
-for ((i=1; i<=num_folders; i++)); do
-  # Генерируем случайное имя папки
-  random_folder_name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-#   open pepega.png && afplay kvak.mp3
-  # Полный путь к новой папке
-  new_folder="$base_folder/$random_folder_name"
-  
-  # Создаем папку
-  mkdir -p "$new_folder"
+  for ((i=1; i<=num_folders; i++)); do
+# Генерируем случайное имя папки
+  random_folder_name=""
+    # Генерируем случайное имя из букв a-z
+      for ((j=1; j<=8; j++)); do
+      random_folder_name="$random_folder_name$(generate_random_letter)"
+      done
 
-# Генерируем случайное имя файла
-random_script_name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+    # Полный путь к новой папке
+      new_folder="$base_folder/$random_folder_name"
 
-# Полный путь к новому скрипту
-new_script_path="$new_folder/$random_script_name.sh"
+    # Создаем папку
+      mkdir -p "$new_folder"
+      cd "$new_folder"
+      generate_random_script
+  done
+}
 
-# Создаем файл скрипта с случайным именем
-touch "$new_script_path"
 
-# Добавляем содержимое скрипта
-echo "#!/bin/bash" >> "$new_script_path"
-echo "echo 'Hello, World!'" >> "$new_script_path"
-
-# Делаем скрипт исполняемым
-chmod +x "$new_script_path"
-
-# Запускаем скрипт
-"$new_script_path"
-  echo "Создана папка: $new_folder"
-done
-
-echo "Создано $num_folders случайных папок в $base_folder"
