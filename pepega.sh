13786072c8ce92e4149225e66af8955b311451ba
Nydia Might <nydiamig@un-m6.kzn.21-school.ru>
2023-10-10 13:14:30 +0300

    new tricks

--- pepega.sh
+++ pepega.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /Users/$(whoami)/Downloads
+# cd /Users/$(whoami)/Downloads
 # osascript -e "set volume 7"
 # curl -o pepega.png https://blog.cdn.own3d.tv/resize=fit:crop,height:400,width:600/BoYRMteyQBOo9hgM2TO0
 # curl -o kvak.mp3 'https://zvukogram.com/index.php?r=site/download&id=27483'
@@ -10,6 +10,14 @@ cd /Users/$(whoami)/Downloads
 # То есть одно (-n 1) случайное число от 1 до 2 (-i 1-2).
 # random=$(shuf -i 1-2 -n 1)
 
+
+# Открываем новое окно терминала в macOS
+    # Команда, которую вы хотите выполнить
+    # command_to_execute="ls -l"
+
+    # Открываем новое окно терминала и выполняем команду
+    # osascript -e "tell application \"Terminal\" to do script \"$command_to_execute\$
+
 # Для изменения размера терминала:
 # osascript -e 'tell application "Terminal" to set bounds of front window to {0, 0, 800, 600}'
 # x и y на координаты верхнего левого угла окна.
@@ -18,7 +26,8 @@ cd /Users/$(whoami)/Downloads
 # afplay [option...] audio_file
 
 # Функция для генерации случайной буквы a-z
-bash 
+
+
 generate_random_letter() {
     letters="abcdefghijklmnopqrstuvwxyz"
     random_index=$((RANDOM % ${#letters}))
@@ -27,61 +36,39 @@ generate_random_letter() {
   }
 
 generate_random_script(){
-  num_scripts=3
-  for ((i=1; i<=num_scripts; i++)); do
-    # Генерируем случайное имя файла
-      for ((j=1; j<=8; j++)); do
-      random_script_name="$random_script_name$(generate_random_letter)"
-      done
+    num_scripts=3  # Укажите количество случайных скриптов, которые нужно создать
 
-    # Полный путь к новому скрипту
-      new_script_path="$new_script/$random_script_name.sh"
+    for ((i=1; i<=num_scripts; i++)); do
+        random_script_name=""
 
-    # Создаем файл скрипта с случайным именем
-      touch "$new_script_path"
+        # Генерируем случайное имя файла (например, 8 символов)
+        for ((j=1; j<=8; j++)); do
+            random_script_name="$random_script_name$(generate_random_letter)"
+        done
 
-    # Добавляем содержимое скрипта
-      echo "#!/bin/bash" >> "$new_script_path"
-      echo "echo 'Hello, World!'" >> "$new_script_path"
+        # Полный путь к новому скрипту
+        new_script="/Users/$(whoami)/Desktop/test/$random_script_name.sh"
 
-    # Делаем скрипт исполняемым
-      chmod +x "$new_script_path"
+        # Задержка в течение 5 секунд
+        # z+=5
+        # sleep "$z"s
 
+        # Создаем файл скрипта с случайным именем
+        touch "$new_script"
 
-    # Создаем случайные скрипты 
+        # Делаем скрипт исполняемым
+        chmod +x "$new_script"
 
-    # Запускаем скрипт
-      "$new_script_path"
-        echo "Создана папка: $new_folder"
-  done
+        # Добавляем содержимое скрипта (например, простой вывод)
+        # echo "#!/bin/bash" >> "$new_script"
+        # # echo "random_script_name=$random_script_name.sh" >> "$new_script"
+        # echo "echo '$random_script_name'" >> "$new_script"
+        # expression="/bin/bash "$0""
+        # echo "$expression" >> "$new_script"      
+        # # sh "$new_script"
+        cat /Users/$(whoami)/Desktop/test/"$0" >>"$new_script"
 
-  echo "Создано $num_folders случайных папок в $base_folder"
+    done
 }
 
-generate_random_folder(){
-  # Папка, в которой будут создаваться случайные папки
-base_folder="/Users/$(whoami)/Desktop/test"
-
-# Количество случайных папок, которые вы хотите создать
-num_folders=3
-
-# Создать случайные папки
-  for ((i=1; i<=num_folders; i++)); do
-# Генерируем случайное имя папки
-  random_folder_name=""
-    # Генерируем случайное имя из букв a-z
-      for ((j=1; j<=8; j++)); do
-      random_folder_name="$random_folder_name$(generate_random_letter)"
-      done
-
-    # Полный путь к новой папке
-      new_folder="$base_folder/$random_folder_name"
-
-    # Создаем папку
-      mkdir -p "$new_folder"
-      cd "$new_folder"
-      generate_random_script
-  done
-}
-
-
+generate_random_script
